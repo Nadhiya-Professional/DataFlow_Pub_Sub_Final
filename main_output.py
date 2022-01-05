88d6c9b3496af100b62580f9451600f919c89e7a
Nadhiya-Professional <58593282+Nadhiya-Professional@users.noreply.github.com>
2022-01-05 02:06:14 -0600

    Update main_output.py

--- main_output.py
+++ main_output.py
@@ -251,4 +251,4 @@ if __name__ == '__main__':
 #         order_final | "Write to PubSub" >> beam.io.WriteToPubSub(topic="projects/york-cdf-start/topics/dataflow-order"
 #                                                                        "-stock-update",
 #                                                                  with_attributes=False)
-        print("done first table1")
+        print("done cus data flow")
